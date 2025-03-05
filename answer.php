819c59ab040fb75d08b9870707bc429e570c2a15
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-05 18:30:08 +0000

    Image added, Built Up Answer PHP, Index Fix UP

--- answer.php
+++ answer.php
@@ -26,16 +26,20 @@
 
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header"></div>
-    <header class="mdl-layout__header-row">
+    <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Button Sim</span>
+        <span class="mdl-layout-title">Button Sim Pg.2</span>
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="page-content">
-        Press or Push, if you put in a number
+      <div class="right image">
+        <img src="./images/ICD2O-Unit-2-01-Sasha-OK-bit/images/kisspng-javascript-web-applications-scalable-vector-graphi-1713908433870.jpg
       </div>
-      <div class="page-content-answer">CLICKIE CLICK.</div>
+      <div class="page-content">
+        <?php
+        echo "Hello World";
+        ?>
+      </div>
       <br />
       <!--Simple Textfield for integers-->$_COOKIE
       <form action="answer.php" method="GET">
@@ -57,5 +61,3 @@
     </main>
   </div>
 </body>
-
-</html>
